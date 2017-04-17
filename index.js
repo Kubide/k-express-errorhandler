0c696744561fc4b2b90ba31fac46705b286d2570
gelito <angel.quesada@kubide.es>
2017-04-17 10:32:22 +0200

    Reorder parsers to put the standard error parser the last.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = require('./lib/errorhandler-express');
 
+require('./lib/parsers/error.parser');
 require('./lib/parsers/config.parser');
 require('./lib/parsers/mongoose.parser');
-require('./lib/parsers/error.parser');
